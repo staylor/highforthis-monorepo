2ad2af52bf1b1ddecc1f71d3a52316d4c3448ffb
Scott Taylor <scott.c.taylor@mac.com>
2024-06-27 12:45:39 -0400

    use runtime polyfill

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
             reactForget,
             {
               compilationMode: 'infer',
+              enableUseMemoCachePolyfill: true,
               panicThreshold: 'NONE',
             },
           ],
