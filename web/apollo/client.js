1872446fd451ff63dcf560d654a3e7020bd9d88a
Scott Taylor <scott.c.taylor@mac.com>
2026-02-26 21:38:25 -0500

    Fix Apollo Client 4: uri option no longer creates HttpLink
    In AC4, passing 'uri' to ApolloClient no longer auto-creates an HttpLink.
    Must explicitly use: link: new HttpLink({ uri })

--- web/apollo/client.js
+++ web/apollo/client.js
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core';
 
 import fragmentMatcher from './fragmentMatcher.js';
 import typePolicies from './typePolicies.js';
@@ -6,7 +6,7 @@ import typePolicies from './typePolicies.js';
 function factory(uri) {
   return () =>
     new ApolloClient({
-      uri,
+      link: new HttpLink({ uri }),
       cache: new InMemoryCache({
         possibleTypes: fragmentMatcher.possibleTypes,
         typePolicies,
