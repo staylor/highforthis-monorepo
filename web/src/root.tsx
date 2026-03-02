33f0efabddc4aa6a5472e533dc751b26bb87bf96
Scott Taylor <scott.taylor@shopify.com>
2026-03-02 14:29:15 -0500

    Update root.tsx

--- web/src/root.tsx
+++ web/src/root.tsx
@@ -25,7 +25,7 @@ export const links: LinksFunction = () => {
   return [
     { rel: 'preconnect', href: 'https://storage.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
     {
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
